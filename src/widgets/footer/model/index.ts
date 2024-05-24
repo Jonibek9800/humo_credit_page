3c96ddd1bb2cd294ccbf0911bd518c4d9105c328
Jonibek M. Mahmudov <MahmudovJ@HUMO.local>
2024-05-24 17:56:14 +0500

    add footer in page

---
+++ src/widgets/footer/model/index.ts
@@ -0,0 +1,32 @@
+const links = [
+  {
+    label: "Хумо Онлайн",
+    link: "",
+  },
+  {
+    label: "Вакансии",
+    link: "",
+  },
+  {
+    label: "Карта Visa",
+    link: "",
+  },
+  {
+    label: "Кредиты",
+    link: "",
+  },
+  {
+    label: "Вклад — Сарчашма",
+    link: "",
+  },
+  {
+    label: "Валюты",
+    link: "",
+  },
+  {
+    label: "Денежные переводы",
+    link: "",
+  },
+];
+
+export { links };
--- src/widgets/layout/WrapperLayout.tsx
+++ src/widgets/layout/WrapperLayout.tsx
@@ -1,14 +1,18 @@
 import { ReactNode } from "react";
 import styles from "./layout.module.css";
 import HeaderWidget from "../header/ui/Header";
+import { Footer } from "../footer";
 
 const WrapperLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className={styles.container}>
-      <HeaderWidget />
-      {children}
-      {/* <FooterWidget /> */}
-    </div>
+    <>
+      <div className={styles.container}>
+        <HeaderWidget />
+        {children}
+        {/* <FooterWidget /> */}
+      </div>
+      <Footer />
+    </>
   );
 };
 
